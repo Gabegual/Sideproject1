aaf4c0a8a142b7b5fd8d9828b6aa38f4e055285b
Gabriel Gualteros <Roblox4@gmail.com>
2021-07-23 16:47:32 -0400

    change to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import  
 
 function App() {
   return (
