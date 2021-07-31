cfb3862edfc7965e8f44efcf24f83eedb0694c3d
Gabriel Gualteros <Roblox4@gmail.com>
2021-07-31 17:15:48 -0400

    third update added side bar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
 import React from 'react';
+import { Home } from './Home';
+import { About } from './About';
+import { NoMatch } from './NoMatch';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
       <React.Fragment>
         <Router>
           <NavigationBar />
+          <Sidebar />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
